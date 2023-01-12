4b7cce9d13394f7afa004a593a48c40c136b4184
vasekhervert <vasekhervert@gmail.com>
2023-01-12 18:05:54 +0100

    made chars as cards

--- components/Character.tsx
+++ components/Character.tsx
@@ -1,3 +1,5 @@
+import Card from "react-bootstrap/Card";
+
 import Image from "next/image";
 
 interface Char {
@@ -15,10 +17,18 @@ interface Char {
 const Character = ({ props }: Char) => {
   const { id, name, species, image, origin } = props;
   return (
-    <div>
-      <Image alt={`${name}'s Avatar`} src={image} width={100} height={100} />
-      {name}
-    </div>
+    <Card className="mb-4" style={{ cursor: "pointer" }}>
+      <Card.Body>
+        <Image
+          alt={`${name}'s Avatar`}
+          src={image}
+          width={100}
+          height={100}
+          className="m-auto d-block"
+        />
+        <Card.Title className="text-center mt-2">{name}</Card.Title>
+      </Card.Body>
+    </Card>
   );
 };
 
