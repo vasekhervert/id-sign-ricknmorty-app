5ee1bdb6ee62bd3aa37082dddfc79f4d8e3933e7
vasekhervert <vasekhervert@gmail.com>
2023-01-17 15:41:43 +0100

    added revalidation route

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+type Data = {
+  error?: string;
+  message?: string;
+  revalidated?: boolean;
+}
+
+export default async function revalidate(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+    
+    const revalidatePath = req.query.path === undefined ? '' : Array.isArray(req.query.path) ? req.query.path[0]  : req.query.path;
+    
+      if (req.query.secret !== process.env.REVALIDATION_SECRET) {
+        return res.status(401).json({
+          message: 'Invalid token',
+        })
+      }      
+    
+      try {
+        console.log('im trajin')
+        await res.revalidate(revalidatePath)
+        return res.json({
+          revalidated: true,
+        })
+      } catch (err) {
+        console.log('zer iz ej err')
+        return res.status(500).send({error: 'Error revalidating'})
+      }
+}
