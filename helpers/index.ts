9758197c58722eef9790de04f44e929fbf7650ee
vasekhervert <vasekhervert@gmail.com>
2023-01-17 13:08:53 +0100

    added postComment func

--- helpers/index.ts
+++ helpers/index.ts
@@ -50,3 +50,39 @@ export async function getSingleEpisode(id: string | string[] | undefined){
 export function makeSlug(string: string) {
   return string.replace('-', ' ').replace(/[^a-z\d\s]+/gi, "").split(' ').join('-').toLowerCase();
 }
+
+export function makeDateAndTime(timestamp: number) {
+  const date = new Date(timestamp);
+  const options: Intl.DateTimeFormatOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+  
+  return date.toLocaleDateString('en-US', options) // 
+
+}
+
+export async function postComment (
+  id: string | string[] | undefined,
+  formData: {
+    name?: string;
+    email: string;
+    message: string;
+    publicationConsent: boolean;
+  }
+)  {
+  const timestamp = Date.now();
+  const { publicationConsent, ...restOfForm } = formData; // get rid of consent proprty, dont need it
+  const requestBody = {...restOfForm, timestamp};
+  
+  const response = await fetch(`/api/comments?id=${id}`, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestBody)
+  })
+
+  console.log(process.env.BASE_URL);
+  
+  return response.json();
+};
