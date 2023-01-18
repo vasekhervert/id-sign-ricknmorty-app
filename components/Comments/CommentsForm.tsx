7e272bf49032a4764fc3e9ff952c6b284b50e00b
vasekhervert <vasekhervert@gmail.com>
2023-01-18 16:43:46 +0100

    loading styles

--- components/Comments/CommentsForm.tsx
+++ components/Comments/CommentsForm.tsx
@@ -33,11 +33,12 @@ const CommentSchema = Yup.object().shape({
 
 export default function CommentsForm() {
   const [loading, setLoading] = useState<boolean>(false);
+  const [commentPosted, setCommentPosted] = useState<boolean>(false);
   const router = useRouter();
   const { id } = router.query;
 
   return (
-    <Row>
+    <Row className="position-relative">
       <Col>
         <Formik
           initialValues={{
@@ -47,11 +48,21 @@ export default function CommentsForm() {
             publicationConsent: false,
           }}
           validationSchema={CommentSchema}
-          onSubmit={(values: FormValues) => {
+          onSubmit={(values: FormValues, actions) => {
             setLoading(true);
-            postComment(id, router.asPath, values).then((data) => {
+            postComment(id, router.asPath, values);
+            setTimeout(() => {
               setLoading(false);
-            });
+              setCommentPosted(true);
+              actions.resetForm({
+                values: {
+                  nickname: "",
+                  email: "",
+                  message: "",
+                  publicationConsent: false,
+                },
+              });
+            }, 1500);
           }}
         >
           {({ errors, touched }) => (
@@ -114,6 +125,48 @@ export default function CommentsForm() {
           )}
         </Formik>
       </Col>
+      {loading && (
+        <div className="position-absolute top-0 left-0 bottom-0 right-0 bg-white">
+          <svg
+            className="loading"
+            version="1.1"
+            id="L9"
+            x="0px"
+            y="0px"
+            viewBox="0 0 100 100"
+            enableBackground="new 0 0 0 0"
+          >
+            <path
+              fill="#000"
+              d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
+            >
+              <animateTransform
+                attributeName="transform"
+                attributeType="XML"
+                type="rotate"
+                dur="1s"
+                from="0 50 50"
+                to="360 50 50"
+                repeatCount="indefinite"
+              />
+            </path>
+          </svg>
+        </div>
+      )}
+      {commentPosted && (
+        <div className="position-absolute top-0 left-0 bottom-0 right-0 bg-white">
+          <div className="position-relative text-center">
+            <p className="text-success fw-bold fs-4">Success!</p>
+            <p>Your comment has been posted. Refresh the page to see it.</p>
+            <button
+              className="btn btn-primary"
+              onClick={() => setCommentPosted(false)}
+            >
+              Add another comment
+            </button>
+          </div>
+        </div>
+      )}
     </Row>
   );
 }
