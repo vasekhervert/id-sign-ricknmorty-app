6333cef21b2675159428a520fa7579be691ac69b
vasekhervert <vasekhervert@gmail.com>
2023-01-22 14:50:31 +0100

    character redesign

--- components/Character.tsx
+++ components/Character.tsx
@@ -18,50 +18,52 @@ interface Char {
 
 const Character = ({ props }: Char) => {
   const [shouldShowMoreInfo, setShouldShowMoreInfo] = useState<boolean>(false);
-  const { id, name, species, image, origin, gender } = props;
+  const { name, species, image, origin, gender } = props;
 
   return (
-    <Card
-      className="mb-4"
-      style={{ cursor: "pointer", minHeight: 190 }}
+    <div
+      className="rounded my-2 border"
+      style={{ cursor: "pointer" }}
       onClick={() => {
         setShouldShowMoreInfo(!shouldShowMoreInfo);
       }}
     >
-      <Card.Body>
-        <Image
-          alt={`${name}'s Avatar`}
-          src={image}
-          width={100}
-          height={100}
-          className={`m-auto d-block ${shouldShowMoreInfo && "rounded"}`}
-        />
-        <Card.Title className="text-center mt-2 fs-6">{name}</Card.Title>
-        {shouldShowMoreInfo && (
-          <Card.Text>
-            <span>
-              <FormattedMessage
-                id="characters_species"
-                defaultMessage="Species"
-              />
-              : {species}
-              <br />
-              <FormattedMessage
-                id="characters_origin"
-                defaultMessage="Origin"
-              />
-              : {origin.name}
-              <br />
-              <FormattedMessage
-                id="characters_gender"
-                defaultMessage="Gender"
-              />
-              : {gender}
-            </span>
-          </Card.Text>
-        )}
-      </Card.Body>
-    </Card>
+      <div className="d-flex align-items-center">
+        <div>
+          <Image
+            alt={`${name}'s Avatar`}
+            src={image}
+            width={64}
+            height={64}
+            className={`radius-top-left ${
+              !shouldShowMoreInfo && "radius-bottom-left"
+            }`}
+          />
+        </div>
+        <div className="fs-6 fw-bold px-4">{name}</div>
+      </div>
+      <div className={`p-4 ${shouldShowMoreInfo ? "d-block" : "d-none"}`}>
+        <div>
+          <span>
+            <FormattedMessage
+              id="characters_species"
+              defaultMessage="Species"
+            />
+            : {species}
+            <br />
+            <FormattedMessage
+              id="characters_origin"
+              defaultMessage="Origin"
+            />: {origin.name}
+            <br />
+            <FormattedMessage
+              id="characters_gender"
+              defaultMessage="Gender"
+            />: {gender}
+          </span>
+        </div>
+      </div>
+    </div>
   );
 };
 
