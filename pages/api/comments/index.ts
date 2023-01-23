e6595f717e2e0ffe9f0ec7907393c30ca9e88c45
vasekhervert <vasekhervert@gmail.com>
2023-01-23 14:41:51 +0100

    added logic to revalidate pages in all locales

--- pages/api/comments/index.ts
+++ pages/api/comments/index.ts
@@ -8,29 +8,36 @@ export default async function handler(req: NextApiRequest,
     res: NextApiResponse<Data>) {
 
       const {id} = req.query;
-        const {revalPath, ...restOfBody} = req.body;
-        const content =  {...restOfBody};
-        const uid = content.timestamp; // use comment's timetamp as an UID
-        
-        const filePath = `json/episode-${id}.json`;
-        const revalidatePageCache = () => fetch(`${process.env.BASE_URL}/api/revalidate?secret=${process.env.REVALIDATION_SECRET}&path=${revalPath}`)
+      const {revalPath, locales, defaultLocale, ...restOfBody} = req.body;
+      const content =  {...restOfBody};
+      const uid = content.timestamp; // use comment's timetamp as an UID
+      
+      const filePath = `json/episode-${id}.json`;
+      const revalidatePageCache = () => {
+        // revalidate default lang or just revalidate when no locales provided
+        fetch(`${process.env.BASE_URL}/api/revalidate?secret=${process.env.REVALIDATION_SECRET}&path=${revalPath}`);
+        if(locales) { // if locales is not undefined, filter out defaultLocale and revalidate all locales
+          const filteredLocales = locales.filter((i: string) => i !== defaultLocale);
+          filteredLocales.forEach((locale: string) => fetch(`${process.env.BASE_URL}/api/revalidate?secret=${process.env.REVALIDATION_SECRET}&path=/${locale}${revalPath}`));
+        }          
+      }
 
-        try{
-          if (fs.existsSync(filePath)) { // check if there is a comments file for this episode
-            
-            const prevFileContents = fs.readFileSync(filePath, "utf8"); // if is, read its content
-            const parsedPrevFileContents = JSON.parse(prevFileContents); // parse it           
-            const newFileContents = {[uid]:{...content}, ...parsedPrevFileContents}; // create a new content object, 
-                        
-            fs.writeFile(filePath, JSON.stringify(newFileContents), revalidatePageCache);  
-          } else {
-            
-            fs.writeFile(filePath, JSON.stringify({[uid]: {...content}}), revalidatePageCache);  
-          }
+      try{
+        if (fs.existsSync(filePath)) { // check if there is a comments file for this episode
           
-          res.status(200).send('Success')
-        } catch(err) {
-          res.status(500).send('An error occured.')
+          const prevFileContents = fs.readFileSync(filePath, "utf8"); // if is, read its content
+          const parsedPrevFileContents = JSON.parse(prevFileContents); // parse it           
+          const newFileContents = {[uid]:{...content}, ...parsedPrevFileContents}; // create a new content object, 
+                      
+          fs.writeFile(filePath, JSON.stringify(newFileContents), revalidatePageCache);  
+        } else {
+          
+          fs.writeFile(filePath, JSON.stringify({[uid]: {...content}}), revalidatePageCache);  
         }
+        
+        res.status(200).send('Success')
+      } catch(err) {
+        res.status(500).send('An error occured.')
+      }
 
     }
