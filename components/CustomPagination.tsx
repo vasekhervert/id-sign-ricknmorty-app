b1dda37ae6fca02792f15d15db8f223700aadf43
vasekhervert <vasekhervert@gmail.com>
2023-01-25 11:37:43 +0100

    renderPaginationItems now use const/map instead of for/let

--- components/CustomPagination.tsx
+++ components/CustomPagination.tsx
@@ -2,19 +2,17 @@ import Link from "next/link";
 
 const CustomPagination = (props: { currentPage: number; pages: number }) => {
   const renderPaginationItems = () => {
-    let items = [];
-    for (let number = 1; number <= props.pages; number++) {
-      items.push(
-        <li className="page-item" key={number}>
-          <Link
-            href={number === 1 ? "/" : `/episodes/${number}`}
-            className={`page-link ${number === props.currentPage && "active"}`}
-          >
-            {number}
-          </Link>
-        </li>
-      );
-    }
+    const pagesArray = Array.from({ length: props.pages }, (_, i) => i + 1); // make an array of props.pages length
+    const items = pagesArray.map((i: number) => (
+      <li className="page-item" key={i}>
+        <Link
+          href={i === 1 ? "/" : `/episodes/${i}`}
+          className={`page-link ${i === props.currentPage && "active"}`}
+        >
+          {i}
+        </Link>
+      </li>
+    ));
 
     return items;
   };
